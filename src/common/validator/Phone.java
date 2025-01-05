9b1e29bef2cf9fb23fe65735fedfc76899bd6a17
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-05 14:33:53 +0600

    Added Phone validator class to validate phone number

---
+++ src/common/validator/Phone.java
@@ -0,0 +1,12 @@
+package common.validator;
+
+import java.util.regex.Pattern;
+
+public final class Phone {
+    private static final String PHONE_REGEX = "^(\\+8801|01)[3-9][0-9]{8}$";
+    private static final Pattern PHONE_PATTERN = Pattern.compile(PHONE_REGEX);
+
+    public static boolean validate(String phone) {
+        return PHONE_PATTERN.matcher(phone).matches();
+    }
+}
