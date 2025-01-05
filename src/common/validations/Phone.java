d77ac00816030366383ef965f137313a16bee0f8
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-05 14:46:16 +0600

    Refactored Phone validation class

--- src/common/validations/Phone.java
+++ src/common/validations/Phone.java
@@ -1,12 +1,9 @@
 package common.validations;
 
-import java.util.regex.Pattern;
-
 public final class Phone {
     private static final String PHONE_REGEX = "^(\\+8801|01)[3-9][0-9]{8}$";
-    private static final Pattern PHONE_PATTERN = Pattern.compile(PHONE_REGEX);
 
-    public static boolean validate(String phone) {
-        return PHONE_PATTERN.matcher(phone).matches();
+    public static boolean validate(final String phone) {
+        return Validator.validate(phone, PHONE_REGEX);
     }
 }
