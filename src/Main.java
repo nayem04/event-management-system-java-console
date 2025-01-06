6fd5f485025e502a2295ed3b3ef74b5fe55a4e19
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-06 12:53:49 +0600

    Developed project structure

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import controllers.EventController;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to the Event Management System");
+
+        int option;
+        boolean exit = false;
+        Scanner scanner = new Scanner(System.in);
+        EventController eventController = new EventController();
+
+        while (!exit) {
+            System.out.println("""
+                    \nPlease enter a number between 1 to 5 to choose an option
+                    1. Create a new event
+                    2. Register a new User
+                    3. View Analytics
+                    4. Export Data
+                    5. Exit The Program
+                    """);
+
+            try {
+                option = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a number between 1 to 5 to choose an option");
+                continue;
+            } finally {
+                scanner.nextLine();
+            }
+
+            switch (option) {
+                case 1 -> eventController.createEvent(scanner);
+                case 2 -> eventController.registerUser(scanner);
+                case 3 -> eventController.viewAnalytics();
+                case 4 -> eventController.exportData(scanner);
+                case 5 -> exit = true;
+                default -> System.out.println("Invalid option. Please try again.");
+            }
+        }
+
+        scanner.close();
     }
 }
---
+++ src/controllers/EventController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import java.util.Scanner;
+
+public class EventController {
+    public void createEvent(Scanner scanner){
+    }
+
+    public void registerUser(Scanner scanner){
+    }
+
+    public void viewAnalytics(){
+    }
+
+    public void exportData(Scanner scanner){
+    }
+}
