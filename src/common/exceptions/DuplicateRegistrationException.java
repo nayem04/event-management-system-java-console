2cbe4968baa3b85a170c47e303b0e69e0f82aff6
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-06 10:54:37 +0600

    Added a exception class DuplicateRegistrationException

---
+++ src/common/exceptions/DuplicateRegistrationException.java
@@ -0,0 +1,7 @@
+package common.exceptions;
+
+public class DuplicateRegistrationException extends Exception {
+    public DuplicateRegistrationException(String message) {
+        super(message);
+    }
+}
